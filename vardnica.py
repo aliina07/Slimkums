052b5a94ff38a8220864f03a698eebcbec3308e4
aliina07 <alinasaprovska07@gmail.com>
2023-10-12 19:50:44 +0300

    05.10.2023

--- vardnica.py
+++ vardnica.py
@@ -1,20 +1,30 @@
-#Alīna
-#Saprovska
-#02.10.2023
-#tuples un seta mainīgie
+"""
+Informācija
+"""
 
-t=(1,2,3)
-funkcija=(len(t))
+pirmais = {'atsl1':'vertiba1'
+,
+'atsl2':'vertiba2'}
 
-print(t)
-print(funkcija)
+print(pirmais)
+#Izvadīt konkrētu vērtību, tā tiek pēc atslēgas
+print("Pirmās atslēgas vērtība: ",pirmais['atsl1'])
 
-t[1]
-print()
+#Atslēga vienmēr ir pēdiņās, bet tā neskaitās kā dati
+otrais = {'atsl1':'vertiba1','atsl2':2.5,'atsl3':[1,2,3],'atsl4':{'atsl5':[2,5,7]}}
+#'atsl1':'vertiba1' - tiek glabāti teksta dati
+#'atsl2':2.5 - tiek glabāti decimāldaļas
+#'atsl3':[1,2,3] - tiek glabāts saraksts
+#'atsl4':{'atsl5':[2,5,7]} - tiek glabāta vārdnīca 
+print(otrais)
+print(otrais['atsl3'])
 
+d = {}
+d['vards']='Anna'
+print(d)
+d['vecums']=25
+print(d)
 
-
-count(t)
-
-print(count)
-
+print(pirmais.keys())
+print(pirmais.values())
+print(pirmais.items())
